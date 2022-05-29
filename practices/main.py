8acf528eff5b7e06032c162d113779a876dde446
Odion Akhetuamhen <akhetuamhenodion@gmail.com>
2022-05-29 12:51:53 +0100

    word-count

---
+++ practices/main.py
@@ -0,0 +1,8 @@
+userData = input("do you have money?")
+
+if userData == "Yes":
+    print("can you give me some")
+elif userData =="No":
+    print("ok oo")
+else:
+    print("do you ever agree")
---
+++ word-counter/main.py
@@ -0,0 +1,9 @@
+userSentence = input("enter a sentence: ")
+
+userSentence = userSentence.strip(" ")
+
+listOfWords = userSentence.split(" ")
+
+noOfWords = len(listOfWords)
+
+print(noOfWords)
